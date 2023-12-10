6e83ab715e2fc7f0b2b4efe664c80082a11c1db5
Arturo Rodríguez Padrón <artu.rdzxd@gmail.com>
2023-12-09 18:49:50 -0600

    Admin

--- src/main/java/Logica/Administrador.java
+++ src/main/java/Logica/Administrador.java
@@ -1,5 +1,7 @@
 package Logica;
 
+import java.util.ArrayList;
+
 public class Administrador extends Usuario{
 	
 	public void agregarUsuario(Usuario usuario) {
@@ -7,5 +9,16 @@ public class Administrador extends Usuario{
 	}
 	public void eliminarUsuario(Usuario usuario) {
 		
+	}
+	
+	public void crearGrupo(Profesor profesor, ArrayList<Alumno> alumnos, Materia materia) {
+		
+	}
+	
+	public void crearMateria(Materia materia) {
+		materia.agregarMateria(materia, Administrador.this);
+	}
+	public void eliminarMateria(Materia materia) {
+		materia.eliminarMateria(materia, Administrador.this);
 	}
 }
